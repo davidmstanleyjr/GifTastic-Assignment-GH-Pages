0f0cbb34ad236670eff74fecb78b6b5c2c9e4f25
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-09-19 22:50:56 -0400

    copy and pasted files from original Giftastic repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+var topics = ["Daenerys Targaryen", "Jon Snow", "Jaime Lannister", "Cersei Lannister", "The Hound",
+"Arya Stark", "Night King", "Melisandre", "Joffrey Baratheon", "Tyrion Lannister",];
+
+rNdXDLNYthIjN88XMIVsVMmBK57fR2jJ
